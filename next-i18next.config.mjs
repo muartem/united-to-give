e00b65c542157e01c431cca2ea5cc026adb62d73
artem-mrtnk <a.martyniuk@theclicks.ca>
2025-06-04 19:41:02 -0400

    feat: init

---
+++ next-i18next.config.mjs
@@ -0,0 +1,7 @@
+export const i18nConfig = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr', 'ua'],
+    localeDetection: true,
+  },
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import { i18nConfig } from './next-i18next.config.js'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  ...i18nConfig,
+}
+
+export default nextConfig
