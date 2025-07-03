1d5fbaacd330a15eeb7655aaed05683153820309
muartem <muartem99@gmail.com>
2025-07-03 01:51:03 -0400

    feat: END of Blood

--- src/app/[locale]/blood-donation/page.js
+++ src/app/[locale]/blood-donation/page.js
@@ -20,28 +20,6 @@ export default async function BloodDonationPage() {
 			<FAQ />
 			<DonateOnes />
 			<OtherWays />
-			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolores explicabo ipsa maiores minima
-				obcaecati porro repudiandae, sapiente ullam unde vero voluptatibus. Debitis ducimus et eum fugiat neque numquam
-				porro. Ad alias atque aut cumque, ducimus eaque eos esse excepturi illo iusto minus nesciunt nostrum numquam,
-				odio odit provident quas quisquam sapiente sit sunt tempore ullam velit veniam? Animi doloremque eos, illo
-				officia porro qui quod recusandae veritatis. Aperiam asperiores aut culpa cum, debitis ea explicabo facilis
-				fugiat illum impedit itaque laudantium necessitatibus placeat possimus repudiandae velit veritatis? Illo illum
-				in laboriosam minima quis, repudiandae soluta temporibus ullam unde voluptatem? Asperiores at atque deleniti
-				fuga inventore nisi recusandae sequi. Aliquam consequatur consequuntur dolorem optio pariatur placeat quibusdam
-				totam ullam. Amet autem cumque dolorem esse expedita fuga harum inventore labore, natus nostrum odit, provident
-				quas quidem saepe similique sint tempore. Eum impedit minus nam neque provident? Animi consectetur culpa dicta
-				fugiat in laboriosam necessitatibus numquam officia, tenetur voluptatibus. Aperiam eligendi iusto laudantium
-				omnis quidem. Aliquam amet aperiam blanditiis neque porro, ratione! Accusamus aliquam animi asperiores delectus
-				dignissimos doloremque enim eveniet excepturi expedita, facere fuga iure, minus nam odit officiis possimus
-				soluta temporibus voluptates? Assumenda beatae consequatur cupiditate debitis deserunt distinctio earum eius et
-				excepturi fuga illum ipsa ipsam itaque iure laboriosam magnam maiores maxime, modi numquam obcaecati odio
-				officiis omnis optio pariatur quaerat quia quis quo quod quos recusandae reiciendis rem rerum saepe sed totam
-				unde voluptatem. Culpa, iste, neque. Eius illo ipsa molestias officia quas quo reiciendis similique soluta
-				temporibus. Accusantium aliquam in praesentium reiciendis! Aliquid aut autem consequatur culpa cupiditate error
-				expedita ipsam necessitatibus praesentium ratione recusandae, saepe sit, tempore temporibus ullam veritatis,
-				voluptatem! Animi atque minus nemo vitae? Alias aliquam consequatur corporis, doloribus eaque eligendi enim
-				fugit maxime minus mollitia nemo neque nulla placeat porro repudiandae sint tempora tempore ullam.
-			</div>
 		</>
 	)
 }
