183548588b83f08c102dde3389d57c42c72fc511
muartem <muartem99@gmail.com>
2025-07-04 00:48:32 -0400

    feat: Donate funds page: WhereGoes

--- src/app/[locale]/donate-funds/page.js
+++ src/app/[locale]/donate-funds/page.js
@@ -1,12 +1,14 @@
 import FundsMain from '@/components/templates/Funds/FundsMain'
 import Numbers from '@/components/templates/Funds/Numbers'
+import WhereGoes from '@/components/templates/Funds/WhereGoes'
 
 
 export default async function DonateFundsPage() {
 	return (
 		<>
-			<FundsMain />
-			<Numbers />
+			<FundsMain/>
+			<Numbers/>
+			<WhereGoes/>
 		</>
 	)
 }
