a15b0c740c66ea6e4ccaf94bd34a5b6fac137c2b
muartem <muartem99@gmail.com>
2025-07-04 00:38:19 -0400

    feat: Donate funds page: numbers

--- src/app/[locale]/donate-funds/page.js
+++ src/app/[locale]/donate-funds/page.js
@@ -1,10 +1,12 @@
 import FundsMain from '@/components/templates/Funds/FundsMain'
+import Numbers from '@/components/templates/Funds/Numbers'
 
 
 export default async function DonateFundsPage() {
 	return (
 		<>
 			<FundsMain />
+			<Numbers />
 		</>
 	)
 }
