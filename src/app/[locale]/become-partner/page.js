fac18416b506fe7440deceae27cbd250c5cc953c
muartem <muartem99@gmail.com>
2025-07-14 01:42:23 -0400

    feat: Partner: Good team

--- src/app/[locale]/become-partner/page.js
+++ src/app/[locale]/become-partner/page.js
@@ -1,9 +1,11 @@
+import GoodTeam from '@/components/templates/Partner/GoodTeam'
 import PartnerMain from '@/components/templates/Partner/PartnerMain'
 
 export default async function BloodDonationPage() {
 	return (
 		<>
 			<PartnerMain />
+			<GoodTeam />
 		</>
 	)
 }
