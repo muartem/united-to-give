45ff640c14f0e5c968866d22bcd40557f129ab22
muartem <muartem99@gmail.com>
2025-07-26 00:52:09 -0400

    feat: main:hero translations

--- src/components/templates/Main/MainHero/index.jsx
+++ src/components/templates/Main/MainHero/index.jsx
@@ -1,41 +1,48 @@
 import HeroBg from '@/assets/images/hero-bg.jpg'
 import Button from '@/components/ui/button'
+import {useTranslations} from 'next-intl'
 import Image from 'next/image'
 
 const MainHero = () => {
+  const t = useTranslations('main.hero')
+
   return (
     <main className="md:h-[752px] w-dvw bg-main-gradient md:bg-bw-gradient text-light-black">
       <div className="relative max-w-screen-xl m-auto h-full flex justify-between items-stretch text-center md:text-left flex-col md:flex-row">
         <div className="flex flex-col w-auto md:w-1/2 justify-between pt-16 pb-12 px-6 xl:px-16 md:!pl-0">
           <div>
             <h1 className="uppercase text-black-text mb-0.5 md:mr-16">
-							Join the <span className="text-primary">Blood Donation</span> Initiative
+              {t('join')}
+              <span className="text-primary">{t('bd')}</span>
+              {t('initiative')}
             </h1>
-            <div className="b2 text-light-black mb-6 md:mb-4 md:pr-40">
-							Donate once - save three lives anytime between <span className="text-primary">July 16 to December 16</span>. Help in the way you can.
+            <div className="b2 text-light-black mb-6 md:mb-4 md:pr-36">
+              {t('text1')}
+              <span className="text-primary">{t('date')}</span>
+              {t('text2')}
             </div>
           </div>
           <div>
             <div className="mb-4 md:mb-12">
               <Button variant="primary" classNames="!px-16 !py-4">
-								Donate Blood
+                {t('donateBtn')}
               </Button>
             </div>
             <div className="flex gap-4 md:gap-10 h-auto flex-col md:flex-row">
               <div className="flex-1">
                 <h2 className="text-primary">
-									1000+
+                  {t('donors')}
                 </h2>
                 <div className="b2 text-black-text">
-									Our goal is to attract a thousand of new donors in Toronto.
+                  {t('text3')}
                 </div>
               </div>
               <div className="flex-1">
                 <h2 className="text-primary">
-									$5,000
+                  {t('funds')}
                 </h2>
                 <div className="b2 text-black-text">
-									Collect funds that will be sent to support Ukrainian blood center
+                  {t('text4')}
                 </div>
               </div>
             </div>
@@ -45,31 +52,32 @@ const MainHero = () => {
           <Image src={HeroBg} alt="Hero background" height={568} className="w-full md:w-[50dvw] md:max-w-[50dvw] h-full object-cover object-center absolute top-0 left-0" />
           <div className="flex flex-col pt-16 pb-12 px-6 xl:px-16 relative z-10 w-full">
             <h1 className="uppercase text-background mb-[2px]">
-              <span className="text-primary">Attend</span> charity networking event
+              <span className="text-primary">{t('attend')}</span>
+              {t('charity')}
             </h1>
             <div className="b2 text-background mb-[29px] md:pr-24">
-							Made by Ukrainians, for Ukrainians and friends. Do not hesitate - you know our quality and taste. You&apos;re going to love it.
+              {t('text5')}
             </div>
             <div className="mb-4 md:mb-12">
               <Button variant="event" classNames="!px-16 !py-4">
-								Attend Event
+                {t('attendBtn')}
               </Button>
             </div>
             <div className="flex gap-4 md:gap-10 md:h-[190px] h-auto flex-col md:flex-row">
               <div className="flex-1">
-                <h2 className="text-golden-yellow">
-									3 speakers
+                <h2 className="text-golden-yellow uppercase">
+                  {t('speakers')}
                 </h2>
                 <div className="b2 text-background">
-									People you all know will share their experiences in Canada.
+                  {t('text6')}
                 </div>
               </div>
               <div className="flex-1">
                 <h2 className="text-golden-yellow">
-									500+
+                  {t('attendee')}
                 </h2>
                 <div className="b2 text-background">
-									Event attendees
+                  {t('text7')}
                 </div>
               </div>
             </div>
